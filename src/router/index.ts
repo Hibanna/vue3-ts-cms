ef77f528f01da8d5bdf948cc9007609c9c74eb19
hibanna <pack-cn@outlook.com>
2021-11-10 18:16:22 +0800

    perf(config): add router

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHashHistory} from "vue-router"
+
+const routes: any[] = [
+
+]
+
+const router = createRouter({
+  routes,
+  history:createWebHashHistory()
+});
+
+export default router
