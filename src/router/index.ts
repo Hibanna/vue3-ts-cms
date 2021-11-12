cbdef6f72977a875595414afa24fcf71ae300dda
hibanna <pack-cn@outlook.com>
2021-11-12 17:57:10 +0800

    style(config): modify router config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,19 @@
 import { createRouter, createWebHashHistory} from "vue-router"
+import type { RouteRecordRaw} from "vue-router"
 
-const routes: any[] = [
-
+const routes: RouteRecordRaw[] = [
+    {
+      path:"/"
+      redirect:"/login"
+    },
+    {
+      path : "/login",
+      component: "@/views/login/login.vue"
+    },
+    {
+      path : "/login",
+      component: "@/views/login/login.vue"
+    },
 ]
 
 const router = createRouter({
