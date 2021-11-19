d5772c259cc8e91b988805615fcdcae6bdafb3b7
hibanna <pack-cn@outlook.com>
2021-11-19 17:57:21 +0800

    perf(config): add elementplus

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-
+  <div class="app">
+    <h2>{{ $store.state.name }}</h2>
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-
-
-
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "App",
-  components: {
-  },
-});
+  name: 'App',
+  components: {}
+})
 </script>
 <style lang="less">
 #app {
