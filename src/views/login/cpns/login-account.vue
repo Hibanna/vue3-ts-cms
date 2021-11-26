1d4c0dc78c20e1e91a3b468faf77b136fda41524
hibanna <pack-cn@outlook.com>
2021-11-26 18:11:41 +0800

    feat(login): daily

---
+++ src/views/login/cpns/login-account.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="loginAccount">
+    <el-form label-width="60px">
+      <el-form-item label="账号">
+        <el-input v-model="account.name" />
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="account.password" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const account = reactive({
+      name: '',
+      password: ''
+    })
+    return {
+      account
+    }
+  }
+})
+</script>
+
+<style scoped></style>
--- src/views/login/cpns/login-panel.vue
+++ src/views/login/cpns/login-panel.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="loginPanel">
     <h1 class="title">后台管理系统</h1>
-    <el-tabs type="border-card">
-      <el-tab-pane>
+    <el-tabs type="border-card" stretch>
+      <el-tab-pane name="account">
         <template #label>
-          <span>
-            <el-icon><avatar /></el-icon>账号登录
-          </span>
+          <span><i class="el-icon-user-solid"></i> 账号登录</span>
         </template>
-        账号登录
+        <loginAccount />
       </el-tab-pane>
-      <el-tab-pane>
+      <el-tab-pane name="phone">
         <template #label>
-          <span>
-            <el-icon><cellphone /></el-icon>手机登录
-          </span>
+          <span><i class="el-icon-mobile-phone"></i> 手机登录</span>
         </template>
-        手机登录
+        <loginPhone />
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -24,12 +20,26 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Cellphone, Avatar } from '@element-plus/icons'
+import LoginAccount from './login-account.vue'
+import LoginPhone from './login-phone.vue'
+
 export default defineComponent({
   setup() {
     return {}
+  },
+  components: {
+    LoginAccount,
+    LoginPhone
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.loginPanel {
+  width: 320px;
+  margin-bottom: 150px;
+}
+.title {
+  text-align: center;
+}
+</style>
